b516b1d54c1c5a43fca882ccf642e8fbe213dc0d
FareedKandalaft <fareed@spectrecoder.com>
2021-05-15 12:01:59 -0400

    auto create 16 individual boxes and dynamically sets the background-position xy. magic button toggles 'big' class to trigger rotation transform

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// d
+const magicButton = document.getElementById('btn');
+const boxes = document.getElementById('boxes');
+
+createBoxes();
+
+
+magicButton.addEventListener('click', ()=> {
+  boxes.classList.toggle('big');
+})
+
+
+function createBoxes(){
+  for (let y = 0; y < 4; y++) {
+    for (let x = 0; x < 4; x++) {
+      const box = document.createElement('div');
+      box.classList.add('box');
+      box.style.backgroundPosition = `${-x * 125}px ${-y * 125}px`;
+      boxes.appendChild(box);
+    }
+  }
+}
